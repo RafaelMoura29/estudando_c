476a826ea8e1d1b1a158b858dc5e5076ba1cd694
Rafael Moura <rafaelmoura2929@gmail.com>
2020-04-20 11:26:41 -0300

    aula 3

---
+++ aula3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int args, char *argv[])
+{
+    int numero1 = 13;
+    
+    float numero2 = 22.5;
+
+    char caractere = 'c';
+
+    printf("%d\n", numero1);
+    printf("%.2f\n", numero2);
+    printf("%c\n", caractere);
+
+    return 0;
+}
